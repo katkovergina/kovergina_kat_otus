317153836bd0f26fc6f4d3b33aed15c76a89b420
Kat.Kovergina <katkovergina@gmail.com>
2021-10-20 09:11:06 +0300

    rewrite function sum

--- js1.js
+++ js1.js
@@ -1,11 +1,18 @@
-function sum(term) {
-    this.count = this.count || 0
-    if (term === undefined || term === null) {
-      return this.count
+const curry = fn => {
+    const term = (...args) => {
+        return n => {
+            if(!n) {
+                return args.reduce((count, a) => {
+                    return fn.call(fn, count, a)
+                }, 0)
+            }
+            return term (...args, n)
+        }
     }
+    return term()
+}
 
-    this.count += Number(term)
-    return sum
-  }
+const sum = curry((x, y) => x + y)
+console.log(sum(1)(2)(3)(5)(7)())
 
-  console.log(sum(1)(2)(3)())
+
